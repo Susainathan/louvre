b262fc4e16c9e2a3c4d0eafc778159c4d56f4508
Susainathan <susainathansusai@gmail.com>
2021-10-25 06:05:40 +0530

    added static root

--- Main/settings.py
+++ Main/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
 # Default primary key field type
